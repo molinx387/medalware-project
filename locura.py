090ba406da9349ff4a2d76dfa7c918daba46e916
molinx387 <118855267+molinx387@users.noreply.github.com>
2023-03-27 09:22:42 -0400

    Cambie el nombre del archivo principal

--- locura.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv('detections.csv')
-df2 = pd.read_csv('detections_sha.csv')
-
-concatenated = pd.concat([df1, df2], axis=1, ignore_index=False)
-
-concatenated.to_csv('detections_data.csv', index=False)
