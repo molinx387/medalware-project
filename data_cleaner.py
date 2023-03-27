1978db5bb6b634c13019509f1d9281f5aa51e7fc
molinx387 <118855267+molinx387@users.noreply.github.com>
2023-03-27 09:34:52 -0400

    archivo que va a limpiar el dataset

---
+++ data_cleaner.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import csv  
+import matplotlib.pyplot as plt
+import streamlit as st
+
+data_set = 'C:/Users/Admin/Documents/M/MEDALWARE-PROJECT/detections_data.csv'
+data_reader = pd.read_csv(data_set)
