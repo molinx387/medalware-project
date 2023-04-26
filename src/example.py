5b7c1f010e14d78d2c81adc0b9b79e51ef7190d5
Edkar Chachati <chachati28@gmail.com>
2023-04-25 21:31:37 -0400

    Refactorized example and created files + good practices

---
+++ src/example.py
@@ -0,0 +1,59 @@
+import requests
+import pandas as pd
+
+
+def recent_malware():
+    url = "https://mb-api.abuse.ch/api/v1/"
+    headers = {"API-KEY": "5f93b4ae1962f1fb2c5f67735401f6f3"}W
+    data = {"query": "get_recent", "selector": "100"}
+    malware_data = list()
+
+    malwares = requests.post(
+        url, data=data, timeout=15, headers=headers, allow_redirects=True
+    ).json()
+
+    for malware in malwares["data"]:
+        malware_sha = malware["sha256_hash"]
+        data_sha = {"query": "get_info", "hash": malware_sha}
+        response_sha = requests.post(
+            url, data=data_sha, timeout=15,
+            headers=headers, allow_redirects=True
+        )
+        if response_sha.json()["query_status"] == "hash_not_found":
+            print(">>>>>>  The sample hash was not found on Malbazaar <<<<<<")
+        else:
+            response_json = response_sha.json()
+            df = pd.DataFrame(response_json["data"])
+            df = df[
+                [
+                    "sha256_hash",
+                    "signature",
+                    "file_type",
+                    "file_size",
+                    "delivery_method",
+                    "origin_country",
+                    "first_seen",
+                    "yara_rules",
+                    "vendor_intel",
+                ]
+            ]
+            malware_data.append(df.values.tolist()[0])
+    table_headers = [
+        "SHA256",
+        "Familia",
+        "Tipo Archivo",
+        "cantidad",
+        "Metodo de Entrega",
+        "Origen",
+        "Fecha",
+        "REGLAS YARA",
+        "INFORMES EXTERNOS",
+    ]
+
+    df = pd.DataFrame(malware_data, columns=table_headers)
+    df.to_csv('./example.csv')
+    return df
+
+
+if __name__ == '__main__':
+    recent_malware()
