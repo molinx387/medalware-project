a16bc1611e5a68b304b91c3d875af09aca45b29a
molinx387 <molinx387@gmail.com>
2023-05-02 19:55:07 -0400

    probando cositas

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,15 +1,17 @@
 import pandas as pd
-
+from data_extractor import recent_malware
 
 def cleaner():
-    df = pd.read_csv(open("src/data/example.csv"), index_col=0)
+    df = recent_malware()
     df["Fecha"] = pd.to_datetime(df["Fecha"])
     df["Dia"] = [d.date() for d in df["Fecha"]]
     df["Hora"] = [d.time() for d in df["Fecha"]]
+    df.drop('Fecha', axis=1, inplace=True)
     df["Metodo de Entrega"].fillna("Desconocido", inplace=True)
     df.dropna(inplace=True)
-    df.to_csv("src/data/example.csv")
-
+    df.to_csv("src/data/cleaned.csv")
+    return df
+ 
 
 if __name__ == "__main__":
     cleaner()
