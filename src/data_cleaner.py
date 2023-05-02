895de5bb2b5040cef8a2c02fe82df2cbbdf629d4
JVMP25 <127148428+JVMP25@users.noreply.github.com>
2023-05-02 10:36:19 -0400

    Error en data_cleaner (Listo)

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 
 def cleaner():
-    df = pd.read_csv(open("data/example.csv"), index_col=0)
+    df = pd.read_csv(open("src/data/example.csv"), index_col=0)
     df["Fecha"] = pd.to_datetime(df["Fecha"])
     df["Dia"] = [d.date() for d in df["Fecha"]]
     df["Hora"] = [d.time() for d in df["Fecha"]]
     df["Metodo de Entrega"].fillna("Desconocido", inplace=True)
     df.dropna(inplace=True)
-    df.to_csv("data/cleaned.csv")
+    df.to_csv("src/data/example.csv")
 
 
 if __name__ == "__main__":
