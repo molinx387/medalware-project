ef0ca187f55de7df1717c48d5562de2529795079
molinx387 <molinx387@gmail.com>
2023-05-02 19:55:38 -0400

    probando cositas

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,17 +1,18 @@
 import pandas as pd
 from data_extractor import recent_malware
 
+
 def cleaner():
     df = recent_malware()
     df["Fecha"] = pd.to_datetime(df["Fecha"])
     df["Dia"] = [d.date() for d in df["Fecha"]]
     df["Hora"] = [d.time() for d in df["Fecha"]]
-    df.drop('Fecha', axis=1, inplace=True)
+    df.drop("Fecha", axis=1, inplace=True)
     df["Metodo de Entrega"].fillna("Desconocido", inplace=True)
     df.dropna(inplace=True)
     df.to_csv("src/data/cleaned.csv")
     return df
- 
+
 
 if __name__ == "__main__":
     cleaner()
--- src/data_cluster.py
+++ src/data_cluster.py
@@ -1,18 +1,20 @@
-import os 
-import pandas as pd 
+import os
+import pandas as pd
 from data_cleaner import cleaner
+
 
 def cluster():
     df1 = cleaner()
     df2 = pd.read_csv("src/data/malwares.csv")
-    
-    if os.path.isfile(os.path.join('./data/', 'malwares.csv')):
+
+    if os.path.isfile(os.path.join("./data/", "malwares.csv")):
         for index, row in df1.iterrows():
             if not df2.isin(row).any().any():
                 df2 = df2.append(row)
-        df2.to_csv('src/data/malwares.csv')
+        df2.to_csv("src/data/malwares.csv")
     else:
         df1.to_csv("src/data/malwares.csv")
-cluster()
-        # df.to_csv('my_csv.csv', mode='a', header=False)
 
+
+cluster()
+# df.to_csv('my_csv.csv', mode='a', header=False)
