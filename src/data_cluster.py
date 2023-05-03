3abb8a52dc672189d886530dbd4e3a25cabd0ff2
molinx387 <molinx387@gmail.com>
2023-05-02 22:32:58 -0400

    cluster inconcluso pero alli vamos

--- src/data_cluster.py
+++ src/data_cluster.py
@@ -6,16 +6,10 @@ from data_cleaner import cleaner
 def cluster():
     df1 = cleaner()
     # df2 = pd.read_csv("src/data/malwares.csv")
-    if os.path.isfile(os.path.join("./data/", "malwares.csv")):
-        df2 = pd.read_csv("src/data/malwares.csv")
-        for index, row in df1.iterrows():
-            if not df2.isin(row).any().any():
-                df2 = df2.append(row)
-        df2.to_csv("src/data/malwares.csv")
-    else:
-        df1.to_csv("src/data/malware.csv")
-    return "XD"
+    if os.path.isfile(os.path.join("./data/", "malwares.csv")): 
 
 
-if __name__ == "__main__":
-    cluster()
+
+
+# if __name__ == "__main__":
+    # cluster()
