593bb11a9a903e9cd1849d1302d2fa6a61b8f330
molinx387 <molinx387@gmail.com>
2023-05-03 23:37:56 -0400

    arreglos del cluster

--- src/data_cluster.py
+++ src/data_cluster.py
@@ -5,11 +5,15 @@ from data_cleaner import cleaner
 
 def cluster():
     df1 = cleaner()
-    # df2 = pd.read_csv("src/data/malwares.csv")
-    if os.path.isfile(os.path.join("./data/", "malwares.csv")): 
+    if os.path.exists("malwares.csv") == False:
+        df2 = df1
+        df2.to_csv("src/data/malwares.csv", index=False)
+    else:
+        df2 = pd.read_csv("src/data/malwares.csv", index_col=[0])
+        df3 = pd.concat([df1, df2], ignore_index=True).drop_duplicates()
+        df3.to_csv("src/data/malwares.csv", index=True)
+    return
 
 
-
-
-# if __name__ == "__main__":
-    # cluster()
+if __name__ == "__main__":
+    cluster()
