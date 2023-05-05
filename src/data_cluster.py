d9a3bd60141bf57099371fcf9d791755eb806b49
JVMP25 <127148428+JVMP25@users.noreply.github.com>
2023-05-05 17:02:54 -0400

    data_cluster

--- src/data_cluster.py
+++ src/data_cluster.py
@@ -5,15 +5,15 @@ from data_cleaner import cleaner
 
 def cluster():
     df1 = cleaner()
-    if os.path.exists("malwares.csv") == False:
+    if os.path.isfile("data/malwares.csv") == False:
         df2 = df1
-        df2.to_csv("src/data/malwares.csv", index=False)
+        df2.to_csv("src/data/malwares.csv")
     else:
-        df2 = pd.read_csv("src/data/malwares.csv", index_col=[0])
-        df3 = pd.concat([df1, df2], ignore_index=True).drop_duplicates()
+        df2 = pd.read_csv("src/data/malwares.csv")
+        df3 = pd.concat([df2,df1], ignore_index=True).drop_duplicates()
         df3.to_csv("src/data/malwares.csv", index=True)
     return
 
-
 if __name__ == "__main__":
     cluster()
+
