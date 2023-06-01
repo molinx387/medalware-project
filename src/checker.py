e883552e50d94e1a367b9740b6df521c8e8b55c4
molinx387 <molinx387@gmail.com>
2023-06-01 10:11:17 -0400

    malware_page

---
+++ src/checker.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+from malware_definitions import malware_dict
+
+df = pd.read_csv("src/data/malwares.csv")
+malware_list = df["Malware"].unique()
+
+for malware in malware_list:
+    found = False
+    for malware_dict in malware_dict:
+        if malware == malware_dict["Malware"]:
+            found = True
+            break
+    if not found:
+        print(f"{malware} no se encuentra en ninguna llave Malware.")
